499e00ce46fd04838b0685759502b9826ec67a3c
djhir3 <djhir3@gmail.com>
2020-09-04 17:12:09 +1000

    Edge case fixes and display improvements for swing analyser

--- SwingAnalyser.cpp
+++ SwingAnalyser.cpp
@@ -35,10 +35,10 @@ SwingAnalyser::Output SwingAnalyser::run()
 			[](decltype(election.parties)::value_type party) {return party.second.shortCode == "LP"; });
 		int lpCode = (lpIt != election.parties.end() ? lpIt->second.id : -2);
 		auto lnpIt = std::find_if(election.parties.begin(), election.parties.end(),
-			[](decltype(election.parties)::value_type party) {return party.second.shortCode == "LNP"; });
+			[](decltype(election.parties)::value_type party) {return party.second.shortCode == "LNP" || party.second.shortCode == "LNQ"; });
 		int lnpCode = (lnpIt != election.parties.end() ? lnpIt->second.id : -2);
 		auto natIt = std::find_if(election.parties.begin(), election.parties.end(),
-			[](decltype(election.parties)::value_type party) {return party.second.shortCode == "NAT"; });
+			[](decltype(election.parties)::value_type party) {return party.second.shortCode == "NP"; });
 		int natCode = (natIt != election.parties.end() ? natIt->second.id : -2);
 
 		output.electionNames[electionKey] = election.name;
@@ -47,8 +47,10 @@ SwingAnalyser::Output SwingAnalyser::run()
 			int alpVotes = findVote2cp(election, booth, alpCode);
 			int lpVotes = findVote2cp(election, booth, lpCode) + findVote2cp(election, booth, lnpCode)
 				+ findVote2cp(election, booth, natCode);
-			float alpProportion = (alpVotes && lpVotes ? float(alpVotes) / float(alpVotes + lpVotes) : 0.0f);
-			output.booths[booth.id].elections.insert({ election.id, {alpProportion} });
+			std::optional<float> alpProportion;
+			if (alpVotes) alpProportion = (alpVotes && lpVotes ? float(alpVotes) / float(alpVotes + lpVotes) : 0.0f);
+			auto newVal = output.booths[booth.id].elections.insert({ election.id, {} });
+			if (alpVotes) newVal.first->second.alp2cp = alpProportion;
 		}
 		for (auto const& [seatKey, seat] : election.seats) {
 			for (auto const& boothId : seat.booths) {
@@ -60,12 +62,22 @@ SwingAnalyser::Output SwingAnalyser::run()
 		for (auto firstElection = booth.elections.begin(); firstElection != booth.elections.end(); ++firstElection) {
 			auto secondElection = std::next(firstElection);
 			if (secondElection == booth.elections.end()) break;
+			auto generalFirstElection = std::find_if(elections.begin(), elections.end(), 
+				[&](decltype(*elections.begin()) generalElection) {return generalElection.first == firstElection->first; });
+			if (generalFirstElection == elections.end()) continue;
+			auto generalSecondElection = std::next(generalFirstElection);
+			if (generalSecondElection == elections.end()) continue;
+			if (generalSecondElection->first != secondElection->first) continue;
 			if (firstElection->second.alp2cp && secondElection->second.alp2cp) {
 				secondElection->second.alpSwing = secondElection->second.alp2cp.value() - firstElection->second.alp2cp.value();
 			}
 		}
 	}
 	return output;
+}
+
+inline std::string getFirstWord(std::string const& entire) {
+	return std::string(entire.begin(), entire.begin() + entire.find(" "));
 }
 
 std::string SwingAnalyser::getTextOutput(Output const& data)
@@ -76,11 +88,12 @@ std::string SwingAnalyser::getTextOutput(Output const& data)
 		output << booth.boothName << " (" << booth.seatName << ")" << ": ";
 		bool firstElement = true;
 		for (auto electionIt : booth.elections) {
-			output << (firstElement ? "" : ", ") << data.electionNames.at(electionIt.first);
-			if (electionIt.second.alp2cp) {
+			output << (firstElement ? "" : ", ") << getFirstWord(data.electionNames.at(electionIt.first));
+			if (electionIt.second.alp2cp && electionIt.second.alp2cp.value()) {
 				output << " (ALP 2cp " << electionIt.second.alp2cp.value() * 100.0f << "%";
 				if (electionIt.second.alpSwing) {
-					output << ", " << electionIt.second.alpSwing.value() * 100.0f << "%";
+					output << ", " << (electionIt.second.alpSwing.value() >= 0 ? "+" : "");
+					output << electionIt.second.alpSwing.value() * 100.0f << "%";
 				}
 				output << ")";
 			}
