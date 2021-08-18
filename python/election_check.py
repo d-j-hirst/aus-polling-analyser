9214c6c85e201f0d87f6aa17e1a424352847a1b4
djhir3 <djhir3@gmail.com>
2021-08-19 04:21:04 +1000

    Check for percentage adding to ~100%, plus update for new Morgan poll

--- python/election_check.py
+++ python/election_check.py
@@ -8,6 +8,15 @@ def check_seat_numbers(elections):
         print(f'Election {election.name}: Number of seats is {seat_count}')
 
 
+def check_seat_percent(elections):
+    for code, election in elections.elections.items():
+        for seat_result in election.seat_results:
+            total_percent = sum(x.percent for x in seat_result.fp)
+            if abs(total_percent - 100) > 0.21:
+                print(f'{election.name} - {seat_result.name}: total %: {total_percent}')
+
+
 if __name__ == '__main__':
     elections = AllElections()
     check_seat_numbers(elections)
+    check_seat_percent(elections)
