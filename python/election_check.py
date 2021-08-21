18c31f90693cb50a878d4393208ceb7d313bfcfb
djhir3 <djhir3@gmail.com>
2021-08-21 19:27:40 +1000

    Add check for tcp votes matching percentages

--- python/election_check.py
+++ python/election_check.py
@@ -8,7 +8,7 @@ def check_seat_numbers(elections):
         print(f'Election {election.name}: Number of seats is {seat_count}')
 
 
-def check_seat_fp_percent_total(elections):
+def check_fp_percent_total(elections):
     for code, election in elections.elections.items():
         for seat_result in election.seat_results:
             if len(seat_result.fp) < 2:
@@ -18,7 +18,7 @@ def check_seat_fp_percent_total(elections):
                 print(f'{election.name} - {seat_result.name}: total fp %: {total_percent}')
 
 
-def check_vote_fp_percent_calc(elections):
+def check_fp_percent_calc(elections):
     for code, election in elections.elections.items():
         for seat_result in election.seat_results:
             if len(seat_result.fp) < 2:
@@ -30,7 +30,7 @@ def check_vote_fp_percent_calc(elections):
                     print(f'{election.name} - {seat_result.name} - {candidate.name} - recorded fp %: {candidate.percent}, calc %: {calc_percent}')
 
 
-def check_seat_tcp_percent_total(elections):
+def check_tcp_percent_total(elections):
     for code, election in elections.elections.items():
         for seat_result in election.seat_results:
             if len(seat_result.tcp) < 2:
@@ -46,9 +46,28 @@ def check_seat_tcp_percent_total(elections):
                 print(f'{election.name} - {seat_result.name}: total tcp %: {total_percent}')
 
 
+def check_tcp_percent_calc(elections):
+    for code, election in elections.elections.items():
+        for seat_result in election.seat_results:
+            if len(seat_result.tcp) < 2:
+                # Don't need to flag some old uncontested seats 
+                # or seats where TCP is not recorded
+                continue 
+            if None in [x.percent for x in seat_result.tcp]:
+                # If we are confirmed to not have a valid tcp count,
+                # just skip the seat
+                continue
+            total_votes = sum(x.votes for x in seat_result.tcp)
+            for candidate in seat_result.tcp:
+                calc_percent = candidate.votes / total_votes * 100
+                if abs(calc_percent - candidate.percent) > 0.06:
+                    print(f'{election.name} - {seat_result.name} - {candidate.name} - recorded tcp %: {candidate.percent}, calc %: {calc_percent}')
+
+
 if __name__ == '__main__':
     elections = AllElections()
     check_seat_numbers(elections)
-    check_seat_fp_percent_total(elections)
-    check_vote_fp_percent_calc(elections)
-    check_seat_tcp_percent_total(elections)
+    check_fp_percent_total(elections)
+    check_fp_percent_calc(elections)
+    check_tcp_percent_total(elections)
+    check_tcp_percent_calc(elections)
