a58bfffed962c14cbc3e446c52870f0de8e15572
djhir3 <djhir3@gmail.com>
2021-09-05 22:47:10 +1000

    split election check into actual checking and analysis file
    also update todo list with details for minor/ind analysis

---
+++ python/election_analysis.py
@@ -0,0 +1,153 @@
+from election_code import ElectionCode
+import copy
+import math
+import numpy
+import statistics
+from sklearn.linear_model import LinearRegression
+from scipy.interpolate import UnivariateSpline
+from election_check import get_checked_elections
+from poll_transform import transform_vote_share, detransform_vote_share, clamp
+from sample_kurtosis import one_tail_kurtosis
+
+def analyse_greens(elections):
+    bucket_min = -90
+    bucket_base = 10
+    bucket_max = -30
+    this_buckets = {(-10000, bucket_min): []}
+    this_buckets.update({(a, a + bucket_base): [] for a in range(bucket_min, bucket_max, bucket_base)})
+    this_buckets.update({(bucket_max, 10000): []})
+    swing_buckets = copy.deepcopy(this_buckets)
+    sophomore_buckets = copy.deepcopy(this_buckets)
+    party = 'Greens'
+    for this_election, this_results in elections.items():
+        print(f'Gathering results for {this_election}')
+        if len(elections.next_elections(this_election)) == 0:
+            continue
+        next_election = elections.next_elections(this_election)[0]
+        next_results = elections[next_election]
+        if len(elections.previous_elections(this_election)) > 0:
+            previous_election = elections.previous_elections(this_election)[-1]
+            previous_results = elections[previous_election]
+        else:
+            previous_results = None
+        election_swing = (transform_vote_share(next_results.total_fp_percentage_party(party))
+                 - transform_vote_share(this_results.total_fp_percentage_party(party)))
+        print(f'{this_election.short()} - {next_election.short()} overall swing to greens: {election_swing}')
+        for this_seat_name in this_results.seat_names():
+            this_seat_results = this_results.seat_by_name(this_seat_name)
+            if len(this_seat_results.tcp) == 0:
+                continue  # ignore seats where candidates are unopposed
+            if this_seat_name in next_results.seat_names():
+                next_seat_results = next_results.seat_by_name(this_seat_name)
+                if len(next_seat_results.tcp) == 0:
+                    continue  # ignore seats where candidates are unopposed
+                sophomore = False
+                if (previous_results is not None
+                    and this_seat_name in previous_results.seat_names(include_name_changes=True)):
+                    previous_seat_results = \
+                        previous_results.seat_by_name(this_seat_name,
+                                                      include_name_changes=True)
+                    if (len(previous_seat_results.tcp) > 0 and
+                        previous_seat_results.tcp[0].party != party and 
+                        this_seat_results.tcp[0].party == party):
+                        sophomore = True
+                        print(f'Sophomore found: {this_seat_name}')
+                if party in [a.party for a in this_seat_results.fp]:
+                    this_greens = sum(x.percent for x in this_seat_results.fp
+                                        if x.party == party)
+                else:
+                    continue
+                if party in [a.party for a in next_seat_results.fp]:
+                    next_greens = sum(x.percent for x in next_seat_results.fp
+                                        if x.party == party)
+                else:
+                    continue
+                this_greens = transform_vote_share(this_greens)
+                next_greens = transform_vote_share(next_greens)
+                greens_change = next_greens - this_greens
+                this_bucket = next(a for a in this_buckets 
+                                   if a[0] < this_greens
+                                   and a[1] > this_greens)
+                this_buckets[this_bucket].append(greens_change)
+                swing_buckets[this_bucket].append(election_swing)
+                sophomore_buckets[this_bucket].append(1 if sophomore else 0)
+
+    bucket_counts = {}
+    bucket_swing_coefficients = {}
+    bucket_sophomore_coefficients = {}
+    bucket_intercepts = {}
+    bucket_median_errors = {}
+    bucket_lower_rmses = {}
+    bucket_upper_rmses = {}
+    bucket_lower_kurtoses = {}
+    bucket_upper_kurtoses = {}
+    
+    for bucket, results in this_buckets.items():
+        if len(results) > 0:
+            # Run regression between the seat swing and election swing
+            # to find the relationship between the two for initial primary
+            # votes in this bucket
+            swings = swing_buckets[bucket]
+            sophomores = sophomore_buckets[bucket]
+            inputs_array = numpy.transpose(numpy.array([swings, sophomores]))
+            results_array = numpy.array(results)
+            reg = LinearRegression().fit(inputs_array, results_array)
+            swing_coefficient = reg.coef_[0]
+            sophomore_coefficient = reg.coef_[1]
+            overall_intercept = reg.intercept_
+
+            # Get the residuals (~= errors if the above relationship is used
+            # as a prediction), find the median, and split the errors into
+            # a group above and below the median, measured by their distance
+            # from the median
+            residuals = [results[index] -
+                         (swing_coefficient * swings[index] +
+                          sophomore_coefficient * sophomores[index] 
+                          + overall_intercept)
+                         for index in range(0, len(results))
+            ]
+            median_error = statistics.median(residuals)
+            lower_errors = [a - median_error for a in residuals if a < median_error]
+            upper_errors = [a - median_error for a in residuals if a >= median_error]
+
+            # Find effective RMSE and kurtosis for the two tails of the
+            # distribution (in each case, as if the other side of the
+            # distribution is symmetrical)
+            lower_rmse = math.sqrt(sum([a ** 2 for a in lower_errors])
+                                / (len(lower_errors) - 1))
+            upper_rmse = math.sqrt(sum([a ** 2 for a in upper_errors])
+                                / (len(upper_errors) - 1))      
+            lower_kurtosis = one_tail_kurtosis(lower_errors)
+            upper_kurtosis = one_tail_kurtosis(upper_errors)
+
+            bucket_counts[bucket] = len(results)
+            bucket_swing_coefficients[bucket] = swing_coefficient
+            bucket_sophomore_coefficients[bucket] = sophomore_coefficient
+            bucket_intercepts[bucket] = overall_intercept
+            bucket_median_errors[bucket] = median_error
+            bucket_lower_rmses[bucket] = lower_rmse
+            bucket_upper_rmses[bucket] = upper_rmse
+            bucket_lower_kurtoses[bucket] = lower_kurtosis
+            bucket_upper_kurtoses[bucket] = upper_kurtosis
+            if (bucket[0] == bucket_max):
+                print(swings)
+                print(sophomores)
+                print(results)
+    
+    for bucket in bucket_swing_coefficients.keys():
+        print(f'Primary vote bucket: {detransform_vote_share(bucket[0])} - {detransform_vote_share(bucket[1])}')
+        print(f'Sample size: {bucket_counts[bucket]}')
+        print(f'Election swing coefficient: {bucket_swing_coefficients[bucket]}')
+        print(f'Sophomore coefficient: {bucket_sophomore_coefficients[bucket]}')
+        print(f'Intercept: {bucket_intercepts[bucket]}')
+        print(f'Median error: {bucket_median_errors[bucket]}')
+        print(f'Lower rmse: {bucket_lower_rmses[bucket]}')
+        print(f'Upper rmse: {bucket_upper_rmses[bucket]}')
+        print(f'Lower kurtosis: {bucket_lower_kurtoses[bucket]}')
+        print(f'Upper kurtosis: {bucket_upper_kurtoses[bucket]}')
+        print('\n')
+
+
+if __name__ == '__main__':
+    elections = get_checked_elections()
+    analyse_greens(elections)
--- python/election_check.py
+++ python/election_check.py
@@ -1,13 +1,4 @@
 from election_data import AllElections
-from election_code import ElectionCode
-import copy
-import math
-import numpy
-import statistics
-from sklearn.linear_model import LinearRegression
-from scipy.interpolate import UnivariateSpline
-from poll_transform import transform_vote_share, detransform_vote_share, clamp
-from sample_kurtosis import one_tail_kurtosis
 
 def check_seat_numbers(elections):
     for code, election in elections.elections.items():
@@ -139,156 +130,29 @@ def best_performances(elections):
             print(f'Election: {c[0].region()}{c[0].year()}, Seat: {c[1]}, Name: {c[2]}, fp %: {c[3]}')
 
 
-def analyse_greens(elections):
-    bucket_min = -90
-    bucket_base = 10
-    bucket_max = -30
-    this_buckets = {(-10000, bucket_min): []}
-    this_buckets.update({(a, a + bucket_base): [] for a in range(bucket_min, bucket_max, bucket_base)})
-    this_buckets.update({(bucket_max, 10000): []})
-    swing_buckets = copy.deepcopy(this_buckets)
-    sophomore_buckets = copy.deepcopy(this_buckets)
-    party = 'Greens'
-    for this_election, this_results in elections.items():
-        print(f'Gathering results for {this_election}')
-        if len(elections.next_elections(this_election)) == 0:
-            continue
-        next_election = elections.next_elections(this_election)[0]
-        next_results = elections[next_election]
-        if len(elections.previous_elections(this_election)) > 0:
-            previous_election = elections.previous_elections(this_election)[-1]
-            previous_results = elections[previous_election]
-        else:
-            previous_results = None
-        election_swing = (transform_vote_share(next_results.total_fp_percentage_party(party))
-                 - transform_vote_share(this_results.total_fp_percentage_party(party)))
-        print(f'{this_election.short()} - {next_election.short()} overall swing to greens: {election_swing}')
-        for this_seat_name in this_results.seat_names():
-            this_seat_results = this_results.seat_by_name(this_seat_name)
-            if len(this_seat_results.tcp) == 0:
-                continue  # ignore seats where candidates are unopposed
-            if this_seat_name in next_results.seat_names():
-                next_seat_results = next_results.seat_by_name(this_seat_name)
-                if len(next_seat_results.tcp) == 0:
-                    continue  # ignore seats where candidates are unopposed
-                sophomore = False
-                if (previous_results is not None
-                    and this_seat_name in previous_results.seat_names(include_name_changes=True)):
-                    previous_seat_results = \
-                        previous_results.seat_by_name(this_seat_name,
-                                                      include_name_changes=True)
-                    if (len(previous_seat_results.tcp) > 0 and
-                        previous_seat_results.tcp[0].party != party and 
-                        this_seat_results.tcp[0].party == party):
-                        sophomore = True
-                        print(f'Sophomore found: {this_seat_name}')
-                if party in [a.party for a in this_seat_results.fp]:
-                    this_greens = sum(x.percent for x in this_seat_results.fp
-                                        if x.party == party)
-                else:
-                    continue
-                if party in [a.party for a in next_seat_results.fp]:
-                    next_greens = sum(x.percent for x in next_seat_results.fp
-                                        if x.party == party)
-                else:
-                    continue
-                this_greens = transform_vote_share(this_greens)
-                next_greens = transform_vote_share(next_greens)
-                greens_change = next_greens - this_greens
-                this_bucket = next(a for a in this_buckets 
-                                   if a[0] < this_greens
-                                   and a[1] > this_greens)
-                this_buckets[this_bucket].append(greens_change)
-                swing_buckets[this_bucket].append(election_swing)
-                sophomore_buckets[this_bucket].append(1 if sophomore else 0)
-
-    bucket_counts = {}
-    bucket_swing_coefficients = {}
-    bucket_sophomore_coefficients = {}
-    bucket_intercepts = {}
-    bucket_median_errors = {}
-    bucket_lower_rmses = {}
-    bucket_upper_rmses = {}
-    bucket_lower_kurtoses = {}
-    bucket_upper_kurtoses = {}
-    
-    for bucket, results in this_buckets.items():
-        if len(results) > 0:
-            # Run regression between the seat swing and election swing
-            # to find the relationship between the two for initial primary
-            # votes in this bucket
-            swings = swing_buckets[bucket]
-            sophomores = sophomore_buckets[bucket]
-            inputs_array = numpy.transpose(numpy.array([swings, sophomores]))
-            results_array = numpy.array(results)
-            reg = LinearRegression().fit(inputs_array, results_array)
-            swing_coefficient = reg.coef_[0]
-            sophomore_coefficient = reg.coef_[1]
-            overall_intercept = reg.intercept_
-
-            # Get the residuals (~= errors if the above relationship is used
-            # as a prediction), find the median, and split the errors into
-            # a group above and below the median, measured by their distance
-            # from the median
-            residuals = [results[index] -
-                         (swing_coefficient * swings[index] +
-                          sophomore_coefficient * sophomores[index] 
-                          + overall_intercept)
-                         for index in range(0, len(results))
-            ]
-            median_error = statistics.median(residuals)
-            lower_errors = [a - median_error for a in residuals if a < median_error]
-            upper_errors = [a - median_error for a in residuals if a >= median_error]
-
-            # Find effective RMSE and kurtosis for the two tails of the
-            # distribution (in each case, as if the other side of the
-            # distribution is symmetrical)
-            lower_rmse = math.sqrt(sum([a ** 2 for a in lower_errors])
-                                / (len(lower_errors) - 1))
-            upper_rmse = math.sqrt(sum([a ** 2 for a in upper_errors])
-                                / (len(upper_errors) - 1))      
-            lower_kurtosis = one_tail_kurtosis(lower_errors)
-            upper_kurtosis = one_tail_kurtosis(upper_errors)
-
-            bucket_counts[bucket] = len(results)
-            bucket_swing_coefficients[bucket] = swing_coefficient
-            bucket_sophomore_coefficients[bucket] = sophomore_coefficient
-            bucket_intercepts[bucket] = overall_intercept
-            bucket_median_errors[bucket] = median_error
-            bucket_lower_rmses[bucket] = lower_rmse
-            bucket_upper_rmses[bucket] = upper_rmse
-            bucket_lower_kurtoses[bucket] = lower_kurtosis
-            bucket_upper_kurtoses[bucket] = upper_kurtosis
-            if (bucket[0] == bucket_max):
-                print(swings)
-                print(sophomores)
-                print(results)
-    
-    for bucket in bucket_swing_coefficients.keys():
-        print(f'Primary vote bucket: {detransform_vote_share(bucket[0])} - {detransform_vote_share(bucket[1])}')
-        print(f'Sample size: {bucket_counts[bucket]}')
-        print(f'Election swing coefficient: {bucket_swing_coefficients[bucket]}')
-        print(f'Sophomore coefficient: {bucket_sophomore_coefficients[bucket]}')
-        print(f'Intercept: {bucket_intercepts[bucket]}')
-        print(f'Median error: {bucket_median_errors[bucket]}')
-        print(f'Lower rmse: {bucket_lower_rmses[bucket]}')
-        print(f'Upper rmse: {bucket_upper_rmses[bucket]}')
-        print(f'Lower kurtosis: {bucket_lower_kurtoses[bucket]}')
-        print(f'Upper kurtosis: {bucket_upper_kurtoses[bucket]}')
-        print('\n')
 
 
-if __name__ == '__main__':
+def get_checked_elections():
     elections = AllElections()
-    # check_seat_numbers(elections)
+    # Automatic checks that enforce consistency of election data
     check_fp_percent_total(elections)
     check_fp_percent_match(elections)
     check_fp_percent_calc(elections)
     check_tcp_percent_total(elections)
     check_tcp_percent_match(elections)
+    # Combine micro parties into categories for better data processing
     combine_parties(elections)
+    
+    # Uncomment this to display seat numbers for each election in console
+    # Results are only verified manually, so it's disabled by default
+    # check_seat_numbers(elections)
 
+    # Functions to do some interesting basic analysis, disable by default
     # display_parties(elections)
     # best_performances(elections)
+    
+    return elections
 
-    analyse_greens(elections)
+
+if __name__ == '__main__':
+    get_checked_elections()
